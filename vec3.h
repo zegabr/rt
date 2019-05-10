4e98b3fdc671108ad7a3b81d77dbdb0e58dd6b24
WildClown <gtsa@cin.ufpe.br>
2019-05-10 10:32:26 -0300

    Vai ficar sim vec3 fdp

---
+++ vec3.h
@@ -0,0 +1,147 @@
+//==================================================================================================
+// Written in 2016 by Peter Shirley <ptrshrl@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is distributed
+// without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication along
+// with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//==================================================================================================
+
+#ifndef VEC3H
+#define VEC3H
+
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+class vec3  {
+
+    
+public:
+    vec3() {}
+    vec3(double e0, double e1, double e2) { x = e0; y = e1; z = e2; }
+    
+    inline const vec3& operator+() const { return *this; }
+    inline vec3 operator-() const { return vec3(-x, -y, -z); }
+    inline vec3& operator+=(const vec3 &v2);
+    inline vec3& operator-=(const vec3 &v2);
+    inline vec3& operator*=(const vec3 &v2);
+    inline vec3& operator/=(const vec3 &v2);
+    inline vec3& operator*=(const double t);
+    inline vec3& operator/=(const double t);
+    
+    inline double size() const { return sqrt(x*x + y*y + z*z); }
+    inline double squared_size() const { return x*x + y*y + z*z; }
+    inline void normalize();
+    
+    
+    double x,y,z;
+};
+
+
+
+inline std::istream& operator>>(std::istream &is, vec3 &t) {
+    is >> t.x >> t.y >> t.z;
+    return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
+    os << t.x << " " << t.y << " " << t.z;
+    return os;
+}
+
+inline void vec3::normalize() {
+    double k = 1.0 / sqrt(x*x + y*y + z*z);
+    x *= k; y *= k; z *= k;
+}
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+}
+
+inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
+}
+
+inline vec3 operator*(double t, const vec3 &v) {
+    return vec3(t*v.x, t*v.y, t*v.z);
+}
+
+inline vec3 operator/(vec3 v, double t) {
+    return vec3(v.x/t, v.y/t, v.z/t);
+}
+
+inline vec3 operator*(const vec3 &v, double t) {
+    return vec3(t*v.x, t*v.y, t*v.z);
+}
+
+inline double dot(const vec3 &v1, const vec3 &v2) {
+    return v1.x *v2.x + v1.y *v2.y  + v1.z *v2.z;
+}
+
+inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+    return vec3( (v1.y*v2.z - v1.z*v2.y),
+                (-(v1.x*v2.z - v1.z*v2.x)),
+                (v1.x*v2.y - v1.y*v2.x));
+}
+
+
+inline vec3& vec3::operator+=(const vec3 &v){
+    x  += v.x;
+    y  += v.y;
+    z  += v.z;
+    return *this;
+}
+
+inline vec3& vec3::operator*=(const vec3 &v){
+    x  *= v.x;
+    y  *= v.y;
+    z  *= v.z;
+    return *this;
+}
+
+inline vec3& vec3::operator/=(const vec3 &v){
+    x  /= v.x;
+    y  /= v.y;
+    z  /= v.z;
+    return *this;
+}
+
+inline vec3& vec3::operator-=(const vec3& v) {
+    x  -= v.x;
+    y  -= v.y;
+    z  -= v.z;
+    return *this;
+}
+
+inline vec3& vec3::operator*=(const double t) {
+    x  *= t;
+    y  *= t;
+    z  *= t;
+    return *this;
+}
+
+inline vec3& vec3::operator/=(const double t) {
+    double k = 1.0/t;
+    
+    x  *= k;
+    y  *= k;
+    z  *= k;
+    return *this;
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.size();
+}
+
+#endif
