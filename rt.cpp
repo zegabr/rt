78497ef50d8fbfd5d0af1636741259d0476e66c0
Gabriel Teixeira Soares de Almeida <gtsa@WINDOWS.CIN.UFPE.BR>
2019-05-10 14:49:14 -0300

    Sem tempo irmão: Pag 19.

--- rt.cpp
+++ rt.cpp
@@ -1,10 +1,9 @@
 //#include<bits/stdc++.h> //inclui tudo mas compila mais lento
 #include<iostream>
 #include<fstream>
-#include<vector>
-#include<cmath>
-#include"vec3.h"
-#include"ray.h"
+#include "sphere.h"
+#include "hitablelist.h"
+#include "float.h"
 using namespace std;
 float hit_sphere(const vec3& center, float radius, const ray& r){ // t*t*dot(B,B) ​+ 2*t*dot( B,A - C ​ ) + dot( A-C,A -C ) - R*R = 0
     vec3 oc = r.origin() - center;
@@ -18,36 +17,39 @@ float hit_sphere(const vec3& center, float radius, const ray& r){ // t*t*dot(B,B
         return (-b + sqrt(discriminant)) / (2.0*a);
     }
 }
-vec3 color(const ray& r){
-    float t = hit_sphere (vec3(0,0,-1),0.5,r);//Em vc3, está a posição da horizontal, a altura, e a profundidade respectivamente.
-    if (t > 0.0){
-        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
-        return 0.5*vec3(N.x+1,N.y+1,N.z+1);
+vec3 color(const ray& r, hitable *world){
+    hit_record rec;
+    if(world->hit(r,0.0,FLT_MAX,rec)){
+        return 0.5*vec3(rec.normal.x+1,rec.normal.y+1,rec.normal.z+1);
     }
-    vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5*(unit_direction.y+1.0);
-    return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
+    else{
+        vec3 unit_direction = unit_vector(r.direction());
+        float t= 0.5*(unit_direction.y+1.0);
+        return (1.0-t)*vec3(1.0,1.0,1.0)+t*vec3(0.5,0.7,1.0);
+    }
 }
 
-
-
-
-
 int main(){
-    const int W = 1000;
+    const int W = 500;
     const int H = 500;
-    ofstream out("out.ppm");//arquivo resultado
+    ofstream out("ibagem.ppm");//arquivo resultado
     out << "P3" << '\n' << W << '\n' << H << '\n' << "255" << '\n';
-    vec3 lower_left_corner(-2.0,-1.0,-1.0);
-    vec3 horizontal(4.0,0.0,0.0);
-    vec3 vertical(0.0,2.0,0.0);
+    vec3 lower_left_corner(-5.0,-5.0,-1.0);
+    vec3 horizontal(10.0,0.0,0.0);
+    vec3 vertical(0.0,10.0,0.0);
     vec3 origin(0.0,0.0,0.0);
+    hitable *list[2];
+    list[0] = new sphere(vec3(0,0,-1),0.5);
+    list[1] = new sphere(vec3(0,-100.5,-1),100);
+    hitable * world = new hitable_list(list,2);
     for(int j = H-1; j >= 0; j--){
         for(int i = 0; i < W; i++){
             float u = float(i)/float(W);
             float v = float(j)/float(H);
             ray r(origin, lower_left_corner + v*vertical + u*horizontal);
-            vec3 col = color(r);
+
+            vec3 p = r.point_at_parameter(2.0);
+            vec3 col = color(r,world);
             int ir = int(255.99*col.x);
             int ig = int(255.99*col.y);
             int ib = int(255.99*col.z);
