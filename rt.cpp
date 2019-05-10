e197de818bb22889f6cd0fee07029e9dd299dc14
WildClown <gtsa@cin.ufpe.br>
2019-05-10 11:50:19 -0300

    Adicionado a esfera, e realizado o shading

--- rt.cpp
+++ rt.cpp
@@ -6,9 +6,26 @@
 #include"vec3.h"
 #include"ray.h"
 using namespace std;
+float hit_sphere(const vec3& center, float radius, const ray& r){ // t*t*dot(B,B) ​+ 2*t*dot( B,A - C ​ ) + dot( A-C,A -C ) - R*R = 0
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc,oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    if(discriminant < 0){
+        return 1.0;
+    }else{
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
+}
 vec3 color(const ray& r){
+    float t = hit_sphere (vec3(0,0,-1),0.5,r);//Em vc3, está a posição da horizontal, a altura, e a profundidade respectivamente.
+    if (t > 0.0){
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
+        return 0.5*vec3(N.x+1,N.y+1,N.z+1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5*(unit_direction.y+1.0);
+    t = 0.5*(unit_direction.y+1.0);
     return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
 }
 
@@ -17,7 +34,7 @@ vec3 color(const ray& r){
 
 
 int main(){
-    const int W = 500;
+    const int W = 1000;
     const int H = 500;
     ofstream out("out.ppm");//arquivo resultado
     out << "P3" << '\n' << W << '\n' << H << '\n' << "255" << '\n';
