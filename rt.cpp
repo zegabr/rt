6bcaf79a4d1c1272362d807c75d93b2510742025
Jose Gabriel Silva Pereira <jgsp2@ug2c06.windows.cin.ufpe.br>
2019-05-10 09:40:45 -0300

    template pra gerar imagem em out.ppm

--- rt.cpp
+++ rt.cpp
@@ -1,5 +1,8 @@
 //#include<bits/stdc++.h> //inclui tudo mas compila mais lento
 #include<iostream>
+#include<fstream>
+#include<vector>
+#define pl '\n'
 using namespace std;
 
 
@@ -8,8 +11,20 @@ using namespace std;
 
 
 int main(){
+    const int W = 500;
+    const int H = 500;
+    ofstream out("out.ppm");//arquivo resultado
+    out << "P3" << '\n' << W << '\n' << H << '\n' << "255" << '\n';
 
 
+    for(int i = 0; i < H; i++){
+        for(int j = 0; j < W; j++){
+            //calcular cor de um pixel aqui
+
+            //colocar r g b do pixel no out
+            out << i << ' ' << j << ' ' << j << '\n';
+        }
+    }
 
 
 
