eb26b45191f3ec98389fa29a360f473bdc86cbe2
titi <tsc@cin.ufpe.br>
2019-05-13 10:39:09 -0300

    Adicionando difuso

--- rt.cpp
+++ rt.cpp
@@ -7,10 +7,19 @@
 #include "camera.h"
 using namespace std;
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0*vec3(drand48(),drand48(),drand48()) - vec3(1,1,1);
+    } while (p.squared_size() >= 1.0);
+    return p;
+}
+
 vec3 color(const ray& r, hitable *world){
     hit_record rec;
     if(world->hit(r,0.0,FLT_MAX,rec)){
-        return 0.5*vec3(rec.normal.x+1,rec.normal.y+1,rec.normal.z+1);
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5*color(ray(rec.p, target-rec.p), world);
     }
     else{
         vec3 unit_direction = unit_vector(r.direction());
@@ -18,6 +27,7 @@ vec3 color(const ray& r, hitable *world){
         return (1.0-t)*vec3(1.0,1.0,1.0)+t*vec3(0.5,0.7,1.0);
     }
 }
+
 
 int main(){
     const int W = 500; // tamanho horizontal da tela
@@ -37,7 +47,7 @@ int main(){
                 float u = float(i + drand48()) / float(W);
                 float v = float(j + drand48()) / float(H);
                 ray r = cam.get_ray(u,v);
-                vec3 p = r.point_at_parameter(2.0);
+                vec3 p = r.point_at_parameter(1.0); // não entendi o que é, ta no código do cara
                 col += color(r,world);
             }
             col /= float(ns);
