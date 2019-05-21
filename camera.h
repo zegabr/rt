0718b5427f6e0bdaef1919eb97f29e09eb54bae7
Jose Gabriel Silva Pereira <jgsp2@ug2c14.windows.cin.ufpe.br>
2019-05-21 14:03:03 -0300

    ns menor pra teste e restauracao dracao da camera antiga

--- camera.h
+++ camera.h
@@ -5,18 +5,21 @@
 
 class camera {
     public:
-        camera( float field_of_view,float zplane, float aspect) { // field of view é vertical, não horizontal(depois pesquisar horizontal)
+        camera(vec3 position, vec3 target, vec3 vup, float field_of_view, float aspect) { // field of view é vertical, não horizontal(depois pesquisar horizontal)
             vec3 u, v, w;
             float theta = field_of_view*M_PI/180;
             float half_height = tan(theta/2);
             float half_width = aspect * half_height;
-            lower_left_corner = vec3(-half_width, -half_height, zplane);
-            horizontal = vec3(2*half_width,0.0,0.0);
-            vertical = vec3(0.0,2*half_height,0.0);
-            origin = vec3(0.0,0.0,0.0);
+            origin = position;
+            w = unit_vector(position-target);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+            lower_left_corner = origin - half_width*u - half_height*v - w;
+            horizontal = 2*half_width*u;
+            vertical = 2*half_height*v;
         }
 
-        ray get_ray (float u, float v) { return ray(origin, lower_left_corner + u*vertical + v*horizontal - origin); }
+        ray get_ray (float u, float v) { return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin); }
 
         vec3 origin;
         vec3 lower_left_corner;
