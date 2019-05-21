96ba4818378d2d5e0c266f1f0bc8bada36f35942
Tiago Sousa Carvalho <tsc2@ug2c24.windows.cin.ufpe.br>
2019-05-21 15:34:50 -0300

    Sombra funcionando

--- rt.cpp
+++ rt.cpp
@@ -20,7 +20,6 @@ vec3 phong(const hit_record &hitou, const camera &cam, phongLight light){
     n = hitou.normal; // normal no ponto que hitou
     v = (cam.origin - hitou.p); // view direction
 
-    vec3 b = unit_vector(l);
     l.normalize();
     v.normalize();
     n.normalize();
@@ -41,17 +40,18 @@ vec3 phong(const hit_record &hitou, const camera &cam, phongLight light){
     if(cosine > 0.0) {
        diffuse = Kd * light.color * cosine;
        specular = Ks*light.color*pow(max(0.0f, vr),exponent);
+       
     }
     return  hitou.material.alpha*(ambient + diffuse + specular);
 
 }
 
-vec3 color(const ray& r, hitable *world, const camera &cam, const phongLight &light){
+vec3 color(const ray& r, const hitable *world, const camera &cam, const phongLight &light){
     hit_record rec;
-    if(world->hit(r,0.0000001,FLT_MAX,rec)){ // se acertar algum objeto da imagem, entra nesse if
+    if(world->hit(r,0.00000001,FLT_MAX,rec)){ // se acertar algum objeto da imagem, entra nesse if
         hit_record h;
-        if(world->hit(ray(rec.p + 1.00001*unit_vector(light.position), unit_vector(light.position)), 0.0000001,FLT_MAX, h)) {
-                return vec3(0.0,0.0,0.0);
+        if(world->hit(ray(rec.p, light.position-rec.p), 0.001,FLT_MAX, h)) {
+            return vec3(0.0,0.0,0.0);
         }
         return phong(rec,cam,light);
     }
@@ -64,17 +64,17 @@ vec3 color(const ray& r, hitable *world, const camera &cam, const phongLight &li
 int main(){
     const int W = 500; // tamanho horizontal da tela
     const int H = 500; // tamanho vertical da tela
-    int ns = 50; // precisão do antialiasing
+    int ns = 15; // precisão do antialiasing
     ofstream out("teste.ppm");//arquivo resultado
     out << "P3" << '\n' << W << '\n' << H << '\n' << "255" << '\n'; 
-    hitable *list[4]; // array de objetos na imagem
-    list[0] = new sphere(vec3(0.0,0.5,-1.0),0.7, phongMaterial(vec3(0.0,0.0,1.0), 0.2, 0.5, 0.6, 1.0)); // esfera do centro
+    hitable *list[3]; // array de objetos na imagem
+    list[0] = new sphere(vec3(0.0,1.0,-1.0),0.2, phongMaterial(vec3(0.0,0.0,1.0), 0.2, 0.5, 0.6, 1.0)); // esfera do centro
     list[1] = new sphere(vec3(0.0,-1000.5,-1.0),1000.0, phongMaterial(vec3(0.0,1.0,0.0), 0.2, 0.5, 0.6, 0.8)); // esfera do "chão"
-    list[2] = new sphere(vec3(-1.0,0.5,-1.0), 0.01, phongMaterial(vec3(1.0,1.0,1.0),1.0,1.0,1.0,1.0));
-    list[3] = new sphere(vec3(1.0,0.5,-1.0),0.7, phongMaterial(vec3(1.0,0.0,1.0), 0.2, 0.5, 0.6, 1.0)); // esfera do centro
-    hitable *world = new hitable_list(list,4); // objeto que tem todas as imagens
-    phongLight light(vec3(1.0,1.0,1.0), vec3(-1.0,0.5,-1.0)); // 1 parametro é a cor, segundo é a posição
-    camera cam(vec3(-3.0,3.0,-3.0), vec3(0.0,0.0,0.0), vec3(0.0,1.0,0.0), 90, float(W)/float(H));
+    //list[2] = new sphere(vec3(0.0,2.0,-1.0), 0.01, phongMaterial(vec3(1.0,1.0,1.0),1.0,1.0,1.0,1.0));
+    list[2] = new sphere(vec3(2.0,1.0,-1.0),0.7, phongMaterial(vec3(1.0,0.0,1.0), 0.2, 0.5, 0.6, 1.0)); // esfera do centro
+    hitable *world = new hitable_list(list,3); // objeto que tem todas as imagens
+    phongLight light(vec3(1.0,1.0,1.0), vec3(2.0,1000.0,-1.0)); // 1 parametro é a cor, segundo é a posição
+    camera cam(vec3(0.0,3.0,4.0), vec3(0.0,0.0,0.0), vec3(0.0,1.0,0.0), 90, float(W)/float(H));
     // camera: 1 parametro é a posição da camera, segundo é o alvo, terceiro é o vetor up, quarto é o fov (vertical), quinto é o aspect/ratio
     for(int j = H-1; j >= 0; j--){ // começa a preencher a imagem de cima para baixo
         for(int i = 0; i < W; i++){ // e da esquerda para a direita
